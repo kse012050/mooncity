786a6db2a5c8322a2c7d303900331bb919a8376e
김성은 <kse012050@naver.com>
2022-09-20 12:09:43 +0900

    메인 페이지 JS 구현했다

--- js/common.js
+++ js/common.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
+
+    $(window).scroll(function(){
+        ($(this).scrollTop() > 0) ? $('header').addClass('active') : $('header').removeClass('active');
+    })
+
+    $('header aside > button').click(function(){
+        $('header aside > div').addClass('active');
+    })
+    $('header aside > div button').click(function(){
+        $('header aside > div').removeClass('active');
+    })
+
     var mainSwiper = new Swiper(".mainArea .sliderArea", {
         pagination: {
-          el: ".swiper-pagination",
+            el: ".swiper-pagination",
         },
     });
 
     var commercialSwiper = new Swiper(".commercialArea .sliderArea .swiper", {
-      slidesPerView: 4,
+        slidesPerView: 4,
     });
 });
